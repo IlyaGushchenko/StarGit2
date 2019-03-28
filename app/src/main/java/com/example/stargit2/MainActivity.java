f7357ba171f58084dd45938925f57232f565e7bb
ILYA GUSHENKO <gushchdva@mail.ru>
2019-03-28 12:58:58 +0300

    ПИ3-1_Гущенко_Кликер

--- app/src/main/java/com/example/stargit2/MainActivity.java
+++ app/src/main/java/com/example/stargit2/MainActivity.java
@@ -2,17 +2,50 @@ package com.example.stargit2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+import android.widget.AdapterView;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    int i = 1;
+    TextView mainTextView;
+    Button mainButton;
+    EditText mainEditText;
 
-    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainTextView = findViewById(R.id.main_textview);
+        mainTextView.setText("Нажми на кнопку снизу");
 
-        btn = findViewById(R.id.button);
+        mainButton = findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+
+
+
+
     }
+    @Override
+    public void onClick(View v) {
+        mainTextView.setText("Была нажата " + i + " раз");
+        i++;
+
+    }
+
+
+
+
+
+
 }
